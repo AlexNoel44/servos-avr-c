cf8c2cc241a3d229e81589f6e6e3c083ce4d7623
anoel <147530628+AlexNoel44@users.noreply.github.com>
2024-03-26 09:13:01 -0400

    Add files via upload

---
+++ servo.c
@@ -0,0 +1,180 @@
+/*
+* @file servo.c
+* @author Alexis Noel
+* @version 2.2
+* @date 11 mars 2024
+* @brief Definition des fonctions servant à intéragir avec des servomoteurs.
+*/
+
+#include "servo.h"
+
+//(angle * 4000) / 180) + 1000; //Le 22 vient de la divison de 4000/180, c'ets pour simplifier le calcul, 1000 est le OCR_MIN ce qui veut onc dire que si l'angle est égal à 0 un 1000 de OCR1A est garanti pour donner 500us de Ton.
+#define OCR_VALUE	9999
+#define OCR_MIN		1000
+#define OCR_POS(a) ((a * 22) + OCR_MIN)
+
+volatile uint8_t ind=0;
+
+volatile uint8_t flag=0;
+
+typedef struct
+{
+	uint8_t pinx;
+	uint8_t pos;
+	volatile uint8_t* ddrx;
+	volatile uint8_t* portx;
+}Servo;
+
+Servo servos[SERVO_MAX];
+
+////////////////////////////////////////////////////////////////////////////////"FONCTIONS PRIVÉES"//////////////////////////////////////////////////////////////////////////////////
+
+/*
+* @brief fonction qui attribue un pin à une instance de servo qui se trouve dans le tbaleau servos
+* @param uint8_t servo : index du servomoteur cible, uint8_t pin : pin à attribuer à l'instance de servomoteur cible
+* @return void
+*/
+void _servoSetPin(uint8_t servo, uint8_t pin)
+{
+	servos[servo].pinx=pin;
+}
+
+/*
+* @brief fonction qui retorune le numéro de la pin du servomoteur cible.
+* @param uint8_t servo : index du servomoteur cible.
+* @return uint8_t servos[servo].pinx : numéro de la pin de l'instance du servomoteur cible.
+*/
+uint8_t _servoGetPin(uint8_t servo)
+{
+	return servos[servo].pinx;	
+}
+
+/***************************************************************************************************************************************************************************************************************/
+
+/*
+* @brief fonction qui attribue le Data Direction Register à une instance de servomoteur qui se trouve dans le tableau servos
+* @param uint8_t servo : index du servomoteur cible, volatile uint8_t* ddr : adresse du Data Direction Register attribuée à la pin de signal de l'instance du servomoteur cible.
+* @return void
+*/
+void _servoSetDDR(uint8_t servo, volatile uint8_t* ddr)
+{
+	servos[servo].ddrx=ddr;
+}
+
+/*
+* @brief fonction qui retourne l'addresse DDR de l'instance du servomoteur cible.
+* @param uint8_t servo : index du servomoteur cible.
+* @return volatile uint8_t servos[servo].ddrx : adresse DDR de l'instance du servomoteur cible.
+*/
+volatile uint8_t* _servoGetDDR(uint8_t servo)
+{
+	return servos[servo].ddrx;
+}
+
+/***************************************************************************************************************************************************************************************************************/
+
+/*
+* @brief fonction qui attribue un PORT à l'instance du servomoteur cible grâce à l'adresse du DDR, qui permet donc d'envoyer un signal 1 ou 0 sur la pin de signal du servomoteur cible.
+* @param uint8_t servo : index du servomoteur cible, volatile uint8_t* ddr : adresse du Data Direction Register attribuée à la pin de signal de l'instance du servo cible.
+* @return void
+*/
+void _servoSetPORT(uint8_t servo, volatile uint8_t* ddr)
+{
+	switch ((uintptr_t)ddr)
+	{
+		case DDRF_ADDR:
+			servos[servo].portx=&PORTF;
+			break;
+		case DDRE_ADDR:
+			servos[servo].portx=&PORTE;
+			break;
+		case DDRD_ADDR:
+			servos[servo].portx=&PORTD;
+			break;
+		case DDRC_ADDR:
+			servos[servo].portx=&PORTC;
+			break;
+		case DDRB_ADDR:
+			servos[servo].portx=&PORTB;
+			break;
+	}
+}
+
+/*
+* @brief fonction qui retourne l'addresse PORT de l'instance du servomoteur cible.
+* @param uint8_t servo : index du servomoteur cible.
+* @return volatile uint8_t servos[servo].portx : adresse PORT de l'instance du servomoteur cible.
+*/
+volatile uint8_t* _servoGetPORT(uint8_t servo)
+{
+	return servos[servo].portx;
+}
+
+/***************************************************************************************************************************************************************************************************************/
+
+void _servoSetPos(uint8_t servo, uint8_t pos)
+{
+	if(pos<=180 && pos>=0)
+		servos[servo].pos=pos;
+}
+
+/***************************************************************************************************************************************************************************************************************/
+
+uint8_t _servoGetPos(uint8_t servo)
+{
+	return servos[servo].pos;
+}
+
+////////////////////////////////////////////////////////////////////////////////"FONCTIONS PUBLIQUES"/////////////////////////////////////////////////////////////////////////////////
+
+void servoInit(uint8_t servoIndex, uint8_t pin, volatile uint8_t* ddr)
+{	
+	_servoSetPin(servoIndex, pin);									
+	_servoSetDDR(servoIndex, ddr);	
+	*(_servoGetDDR(servoIndex)) |= (1<<_servoGetPin(servoIndex));
+	
+	TCCR1B |= (1<<CS11) | (1<<WGM12);	//Prescaler de 8 (CS11)
+	TCNT1=0;
+	OCR1A = 9999;						//200Hz
+	TIMSK1 |= (1<<OCIE1A);				//Démasquement de l'interruption.
+	sei();								//Activation des interruptions.
+
+	_servoSetPORT(servoIndex, _servoGetDDR(servoIndex));
+}
+
+/***************************************************************************************************************************************************************************************************************/
+
+void servoSetAngle(uint8_t servoIndex, uint8_t angle)
+{
+	_servoSetPos(servoIndex, angle);
+}
+
+///////////////////////////////////////////////////////////////////////////VECTEUR D'INTERRUPTION DU TIMER 1//////////////////////////////////////////////////////////////////////////
+
+/*
+* @brief Vecteur d'interruption du TIMER 1. Qaund le flag=1, La pin de l'instance du servomoteur cible est mise à 1 et la durée de temps y est aussi determinée.
+* Donc cette durée sera determiné dans ce if, ensuite comme le flag=0 nous passerons dans le else. Dans le esle il est determiné la durée pour laquelle la pin sera mise à 0 .
+* Par exemple, si l'angle est de 180 degrés, un servomoteur normal aura besoin d'une pulsation durant au moins 2,5ms. Donc, quand le flag est à 1 et que l'on rentre dans l'interruption,
+* Le if sera executé et fixera une la durée avant une nouvelle interruption à 2,5ms. Apres, le 2,5ms, comme le flag est à 0, le else sera éxécuté. Dans ce eles, il ser afixé la durée
+* pour laquelle la pin doit rester à 0. Comme la plupart des servomoteurs marchent à avec une fréquence de 200Hz (5ms) la durée pour cet exemple sera de 5ms-2,5ms donc de 2,5ms.
+* Donc la pin sera allumée 2,5ms et éteinte 2,5ms ce qui fait donc un signal PWM qui contrôle les servomoteurs. Cette opération est répétée pour chaque servmoteurs les uns à la suite
+* des autres.  
+* @param TIMER_COMPA_vect : vecteur d'interruption du timer 1.
+* @return ISR.
+*/
+ISR(TIMER1_COMPA_vect)
+{
+	if (flag)
+	{
+		flag=0;
+		*_servoGetPORT(ind) |= (1<<_servoGetPin(ind));
+		OCR1A = OCR_POS(_servoGetPos(ind));
+	} 
+	else
+	{
+		flag=1;
+		*_servoGetPORT(ind) &= ~(1<<_servoGetPin(ind));
+		OCR1A = OCR_POS(_servoGetPos(ind));
+		ind >= SERVO_MAX-1 ? ind=0 : ind++;
+	}	
+}
---
+++ servo.h
@@ -0,0 +1,58 @@
+/**
+* @file servo.h
+* @author Alexis Noel
+* @version 1.0
+* @date 11 mars 2024
+* @brief Cette bibliothèque offre des fonctions servant à contrôler des servomoteurs avec un AtMega32u4. Si cette bibliothèque est modifiée, elle pourrait être utilisée avec un autre
+* microcontrôleur de la famille AtMega.
+*
+* Pour le faire il suffit de :
+*
+* -Consulter la datasheet du MCU en question.
+* -Modifier les macros contenant les adresses de port propre au AtMega 32u4 puis les remplacer par les adresses du MCU cible.
+* -Ajouter des macros supplémentaires si le MCU cible a plus de registres, puis ajouter ces nouveaux registres dans le switch de la fonction _servoSetPORT.
+* -Modifier les registres d'initialisation du TIMER 1 dans l'init si les noms et conditions changent.
+* -Modifier le nom du vecteur d'interruption du TIMER 1 s'il n'est pas le même sur le MCU cible.
+*
+* La gestion des plusieurs servomoteurs se fait en boucle, à chaque appel de la fonction servoInit dans le main, une nouvelle instance de servmoteur est créée et ajoutée à un tableau.
+* les fonctions s'assure d'atteindre la bonne instance dans le tableau grâce à l'indice du servomoteur en question. Il est préférable de donner 0 comme indice au premier servomoteur 
+* et de continuer à compter de la sorte jusqu'au dernier servomoteur. L'interruption d'un servomoteur tout seul est de 5ms, comme le traitement se fait en boucle, chaque servomoteurs
+* ajoute son 5ms. Donc, pour quatre servomteurs par exemple, chaque servomoteur aura un période réelle de 20ms car 4 servomoteurs * 5 ms de période individuel. Il n'y à pas vraiment
+* de maximum, en tout cas je n'ai jamais fait de test pour le trouver. Donc, théoriquement il pourrait en avoir autant que dans la limite du possible, la seule chose à prendre en
+* compte est le cumul des périodes individuelles de traitement.
+*
+* Le PWM matériel n'est pas utilisé dans cette bibliothèque, c'est donc un PWM logiciel basé sur le TIMER 1 qui place des broches du AtMega 32u4 à 1 ou 0 pour simuler un PWM. 
+*/
+
+#ifndef SERVO_H_
+#define SERVO_H_
+
+#include <avr/io.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
+
+//Nombre de servomoteurs utilisé.
+#define SERVO_MAX	5
+
+//Macros des adresses de Data Direction Register pour les port propres au AtMega 32u4.
+#define DDRF_ADDR 0x30
+#define DDRE_ADDR 0x2D
+#define DDRD_ADDR 0x2A
+#define DDRC_ADDR 0x27
+#define DDRB_ADDR 0x24
+
+/*
+* @brief Initialisation d'un servomoteur, l'instance du servomoteur sera placée dans un tableau avec son numéro de pin et son Data Direction register pour etre traité dans les fonctions.
+* @param uint8_t servoIndex : index du servomoteur individuel, commencer à 0. uint8_t pin : pin utilisée pour le signal du servo sur le DDR. volatile uint8_t* ddr : adresse du Data Direction Register.
+* @return void
+*/
+void servoInit(uint8_t servoIndex, uint8_t pin, volatile uint8_t* ddr);
+
+/*
+* @brief Permet à l'utilisateur de donner au servomoteur un angle entre 0 et 180 degrés.
+* @param void
+* @return void
+*/
+void servoSetAngle(uint8_t servoIndex, uint8_t angle);
+
+#endif
